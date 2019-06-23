66899da579cd670bd2ef9f10fe8e17a259d683cc
kyokang1 <kyoungmokang@gmail.com>
2019-06-23 00:22:53 -0400

    create records

--- app/time_tracker.py
+++ app/time_tracker.py
@@ -1,6 +1,7 @@
 
 import json
 import os
+import datetime
 
 from dotenv import load_dotenv
 import gspread
@@ -19,21 +20,34 @@ scope = [
     'https://www.googleapis.com/auth/drive'
     ]
 
+credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, scope)
+client = gspread.authorize(credentials)
+sheet = client.open("timetracker").sheet1
+
 def get_records():
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, scope)
-    client = gspread.authorize(credentials)
-    sheet = client.open("timetracker").sheet1
     rows = sheet.get_all_records()
     return sheet, rows
 
+def create_records(a, b):
+    response = sheet.insert_row([a, b])
+    return response
+
+#def format_date:
+
 if __name__ == "__main__":
     sheet, rows = get_records()
-    
+
     for row in rows:
         print(row)
     
+    date_input = input("Please input date: ")
+    hour_input = input("Please input hours: ")
+    
+    create_records(str(date_input), float(hour_input))
 
-#def get_records():
+## TODO (as of Jun/22/2019):
+## 1) Insert a new row in the bottom
+## 2) Date should be in date format
 
 
 
@@ -43,44 +57,6 @@ if __name__ == "__main__":
 #add_row()
 
 #def update_row()
-    
-
-#def get_records():
-#    rows = sheet.get_all_values() #> <class 'list'>
-#    return sheet, rows
-
-#print(list(get_records()))
-
-#def create_records():
-#
-#    product = {
-#        "date": next_id,
-#        "hour": product_attributes["name"],
-#    }
-#    next_row = list(product.values()) #> [13, 'Product CLI', 'snacks', 4.99, '2019-01-01']
-#    next_row_number = len(products) + 2 # number of records, plus a header row, plus one
-#    response = sheet.insert_row(next_row, next_row_number)
-#    return response
-
-
-# Extract and print all of the values
-
-
-
-
-
-#list_of_hashes = sheet.get_all_values()
-#list_of_hashes = sheet.row_values(1)
-#list_of_hashes = sheet.col_values(1)
-#list_of_hashes = sheet.cell(1, 1).value
-
-#print(list_of_hashes)
-
-
-
-#doc = client.open_by_key(DOCUMENT_KEY) #> <class 'gspread.models.Spreadsheet'>
-#sheet = doc.worksheet(SHEET_NAME) #> <class 'gspread.models.Worksheet'>
-#rows = sheet.get_all_records() #> <class 'list'>
 
 
 
