65a8d522eca34215bc922028b2984c1e8b0d2987
kyokang1 <kyoungmokang@gmail.com>
2019-06-22 20:46:24 -0400

    requirements & conftest

---
+++ time_tracker.py
@@ -0,0 +1,15 @@
+
+import json
+import os
+
+from dotenv import load_dotenv
+import gspread
+
+from gspread.exceptions import SpreadsheetNotFound
+from oauth2client.service_account import ServiceAccountCredentials
+
+load_dotenv()
+
+
+
+
