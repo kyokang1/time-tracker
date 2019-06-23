4c0573c4e6cd106c6696ac8aa12ec82c8d44f87a
kyokang1 <kyoungmokang@gmail.com>
2019-06-22 22:40:36 -0400

    get record

--- app/time_tracker.py
+++ app/time_tracker.py
@@ -12,9 +12,6 @@ load_dotenv()
 
 CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "auth", "google_api_credentials.json")
 
-#DOCUMENT_KEY = os.environ.get("GOOGLE_SHEET_ID", "OOPS Please get the spreadsheet identifier from its URL")
-#SHEET_NAME = "tracker"
-
 scope = [
     "https://spreadsheets.google.com/feeds",
     "https://www.googleapis.com/auth/spreadsheets", #> Allows read/write access to the user's sheets and their properties.
@@ -22,20 +19,28 @@ scope = [
     'https://www.googleapis.com/auth/drive'
     ]
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, scope)
-client = gspread.authorize(credentials)
-sheet = client.open("timetracker").sheet1
+
+def get_records():
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, scope)
+    client = gspread.authorize(credentials)
+    sheet = client.open("timetracker").sheet1
+    rows = sheet.get_all_values() #> <class 'list'>
+    return sheet, rows
+
 
 
 # Extract and print all of the values
-list_of_hashes = sheet.get_all_records()
+
+
+
+
 
 #list_of_hashes = sheet.get_all_values()
 #list_of_hashes = sheet.row_values(1)
 #list_of_hashes = sheet.col_values(1)
 #list_of_hashes = sheet.cell(1, 1).value
 
-print(list_of_hashes)
+#print(list_of_hashes)
 
 
 
