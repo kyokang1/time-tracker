3347c65da7a67285889c63224420f94b86a61e2e
kyokang1 <kyoungmokang@gmail.com>
2019-06-29 05:44:35 -0400

    from datetime import error fix

--- app/routes.py
+++ app/routes.py
@@ -44,12 +44,11 @@ def results():
     sheet, rows = get_records()
 
     #Define month_id
-    c_year = datetime.now().year
-    c_month = datetime.now().month
-    month_id = str(c_year) + str("_") + str(c_month)
+#    c_year = datetime.now().year
+#    c_month = datetime.now().month
+#    month_id = str(c_year) + str("_") + str(c_month)
     
     #Monthly result
-    
 
 
     #YTD result
--- app/time_tracker.py
+++ app/time_tracker.py
@@ -1,8 +1,8 @@
 
 import json
 import os
+#from datetime import datetime
 import datetime
-from datetime import datetime
 import statistics
 
 from dotenv import load_dotenv
@@ -38,7 +38,7 @@ def create_records(a, b, c, d, e):
         response = sheet.update_cell(cell.row, cell.col+1, float(b))
         response = sheet.update_cell(cell.row, cell.col+2, c)
     else:
-        response = sheet.append_row([a, float(b), c, d, e])
+        response = sheet.append_row([a, float(b), c, int(d), int(e)])
     return response
 
 def day_of_week(d):
@@ -92,28 +92,34 @@ if __name__ == "__main__":
 #    print(list(rows))
     
     #Define month_id
-    c_year = datetime.now().year
-    c_month = datetime.now().month
-    month_id = str(c_year) + str("_") + str(c_month)
-    
-    
-    #r = rows["date"]
-#    print(type(rows))
-#    print(rows)
+#    c_year = datetime.now().year
+#    c_month = datetime.now().month
+#    month_id = str(c_year) + str("_") + str(c_month)
+    print(c_year)
 
-    c_year = 2018
-    c_month = 5
-    rows_year = [r for r in rows if str(r["yyyy"]) == str(c_year)]
-    rows_month = [r for r in rows_year if str(r["mm"]) == str(c_month)]
+#    
+#    #r = rows["date"]
+##    print(type(rows))
+##    print(rows)##
+#    #Calculate - total hour
+#    c_year = 2018
+#    c_month = 5
+#    rows_year = [r for r in rows if str(r["yyyy"]) == str(c_year)]
+#    rows_month = [r for r in rows_year if str(r["mm"]) == str(c_month)]#
+#    rows_year_hr = [r["hour"] for r in rows_year]
+#    rows_month_hr = [r["hour"] for r in rows_month]#
+#    total_hr_ytd = list_total(rows_year_hr)
+#    total_hr_mtd = list_total(rows_month_hr)#
+#    #Calculate - count of weekdays
+##    rows_year_w = [r for r in rows_year if r["dayofweek"] not in ["Mon", "Tue", "Wed", "Thu", "Fri"]]
+#    rows_year_w = [r for r in rows_year if dow_week(r["dayofweek"]) == True]
+#    rows_month_w = [r for r in rows_month if dow_week(r["dayofweek"]) == True]
+##    rows_month = [r for r in rows_year if str(r["mm"]) == str(c_month)]
+#    print(rows_year_w)
+##    print(rows_month_w)
 
-    rows_year_hr = [r["hour"] for r in rows_year]
-    rows_month_hr = [r["hour"] for r in rows_month]
 
-    total_hr_ytd = list_total(rows_year_hr)
-    total_hr_mtd = list_total(rows_month_hr)
 
-    print(total_hr_ytd)
-    print(total_hr_mtd)
     
 
 
